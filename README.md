81ce1f86844771a2d7f4a83b459303ff602b7573
Cweili <3weili@gmail.com>
2013-04-11 10:43:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Seek IP Address in QQWRY.DAT
+===========
+
+* IP database see: <http://www.cz88.net/fox/ipdat.shtml>
+* IP database update at 2013-04-05
+* Source from: <http://code.google.com/p/lumaqq4j/source/browse/trunk/lumaqq/src/edu/tsinghua/lumaqq/>
---
+++ java/org/cweili/ipseeker/Util.java
@@ -0,0 +1,38 @@
+package org.cweili.ipseeker;
+
+import java.io.UnsupportedEncodingException;
+
+public class Util {
+	public static byte[] getIpByteArrayFromString(String ip) {
+		byte[] ret = new byte[4];
+		java.util.StringTokenizer st = new java.util.StringTokenizer(ip, ".");
+		try {
+			ret[0] = (byte) (Integer.parseInt(st.nextToken()) & 0xFF);
+			ret[1] = (byte) (Integer.parseInt(st.nextToken()) & 0xFF);
+			ret[2] = (byte) (Integer.parseInt(st.nextToken()) & 0xFF);
+			ret[3] = (byte) (Integer.parseInt(st.nextToken()) & 0xFF);
+		} catch (Exception e) {
+		}
+		return ret;
+	}
+
+	public static String getString(byte[] b, int offset, int len, String encoding) {
+		try {
+			return new String(b, offset, len, encoding);
+		} catch (UnsupportedEncodingException e) {
+			return new String(b, offset, len);
+		}
+	}
+
+	public static String getIpStringFromBytes(byte[] ip) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(ip[0] & 0xFF);
+		sb.append('.');
+		sb.append(ip[1] & 0xFF);
+		sb.append('.');
+		sb.append(ip[2] & 0xFF);
+		sb.append('.');
+		sb.append(ip[3] & 0xFF);
+		return sb.toString();
+	}
+}
